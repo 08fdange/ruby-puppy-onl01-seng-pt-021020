7266a3aa1901f123b0cd11d6df7653530a6f8098
Frank  D'Angelo <08fdange@gmail.com>
2020-02-23 19:12:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,20 @@
 class Dog 
   @@all = []
   
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
   def self.all
     @@all
   end
+  
+  def self.clear_all
+    @@all = []
+  end
+  
+  def self.print_all
+    
   
   
